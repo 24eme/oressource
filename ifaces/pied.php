0f4270e04d6af9a56e6353d6992a00742a899dcb
tale-fau <tale-fau@24eme.fr>
2025-11-20 18:02:29 +0100

    Mise a jour footer

--- ifaces/pied.php
+++ ifaces/pied.php
@@ -1,8 +1,7 @@
 </div>
-<div class="container">
-  <footer>
-
+  <footer class="footer text-center text-muted" style="margin-top: 10px;">
+    <a href="https://github.com/24eme/oressource">ORessource</a> - Logiciel libre
+    <span class="d-none d-md-inline">sous licence AGPL-3.0 : <a href="https://github.com/24eme/oressource">voir le code source</a></span>
   </footer>
-</div>
 </body>
 </html>
