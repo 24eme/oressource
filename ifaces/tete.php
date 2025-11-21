a9c397e0b71d6c67bf0344ff72197b39f5da23db
tale-fau <tale-fau@24eme.fr>
2025-11-21 12:50:57 +0100

    On remet la mention point de vente sinon on voit plus rien

--- ifaces/tete.php
+++ ifaces/tete.php
@@ -85,7 +85,7 @@ if (array_key_exists('QUERY_STRING', $_SERVER) && str_contains($_SERVER['QUERY_S
             <?php if (is_allowed_vente()):?>
               <?php if (count(filter_visibles(points_ventes($bdd))) < 2):?>
                 <li class="<?php if ($activeTab == 'ventes.php'): ?>active<?php endif; ?>">
-                  <a href="../ifaces/ventes.php?numero=<?php echo filter_visibles(points_ventes($bdd))[0]['id'] ?>"></a>
+                  <a href="../ifaces/ventes.php?numero=<?php echo filter_visibles(points_ventes($bdd))[0]['id'] ?>">Point de vente</a>
                 </li>
               <?php else: ?>
               <li class="nav navbar-nav dropdown<?php if ($activeTab == 'ventes.php'): ?> active<?php endif; ?>">
